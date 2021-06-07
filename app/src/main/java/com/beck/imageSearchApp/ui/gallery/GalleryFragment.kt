2290775b607b5bc06e78c47e60586539f44f60c1
Beck0797 <bekhzod.abduraupov@mail.ru>
2021-06-07 19:46:10 +0900

    Hilt, injected dependencies

--- app/src/main/java/com/beck/imageSearchApp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/beck/imageSearchApp/ui/gallery/GalleryFragment.kt
@@ -2,6 +2,8 @@ package com.beck.imageSearchApp.ui.gallery
 
 import androidx.fragment.app.Fragment
 import com.beck.imageSearchApp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 }
